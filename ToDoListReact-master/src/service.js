3b274f4ea51bc263cdeef77b30b0aeea9330293d
RivkiLieberman <116269616+RivkiLieberman@users.noreply.github.com>
2025-02-04 22:48:14 +0200

    enviroment

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 
-axios.defaults.baseURL = REACT_APP_API_URL
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 axios.interceptors.response.use(
   response => response, 
